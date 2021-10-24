4caa1e9c58d517a65da4489d6c12faabc1ba0d1f
Andrei Chirkov <aachirkov@me.com>
2021-10-24 23:57:42 +0300

    Описание моделей для review и top-page

--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,9 +1,21 @@
-export class ReviewModel {
-  _id: string
-  name: string
-  title: string
-  description: string
-  rating: number
-  createdAt: Date
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+import { prop } from '@typegoose/typegoose';
 
+export interface ReviewModel extends Base {}
+export class ReviewModel extends TimeStamps {
+
+  @prop()
+  name: string
+
+  @prop()
+  title: string
+
+  @prop()
+  description: string
+
+  @prop()
+  rating: number
+
+  @prop()
+  createdAt: Date
 }
--- src/top-page/top-page.model.ts
+++ src/top-page/top-page.model.ts
@@ -1,3 +1,6 @@
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+import { prop } from '@typegoose/typegoose';
+
 export enum TopLevelCategory {
   Courses,
   Services,
@@ -5,26 +8,63 @@ export enum TopLevelCategory {
   Products
 }
 
-export class TopPageModel {
-  _id: string
-  //первый уровень категории (курсы, сервисы, книги, товары)
-  firstCategory: TopLevelCategory
-  //второй уровень категорий (разработка, аналитика, маркетинг)
-  secondCategory: string
+export class HhData {
+  @prop()
+  count: number
+
+  @prop()
+  juniorSalary: number
+
+  @prop()
+  middleSalary: number
+
+  @prop()
+  seniorSalary: number
+}
+
+export class TopPageAdvantage {
+  @prop()
   title: string
+
+  @prop()
+  description: string
+}
+
+export interface TopPageModel extends Base {}
+export class TopPageModel extends TimeStamps{
+
+
+  //первый уровень категории (курсы, сервисы, книги, товары)
+  @prop({ enum: TopLevelCategory })
+  firstCategory: TopLevelCategory
+
+  //второй уровень категорий (разработка, аналитика, маркетинг)
+  @prop()
+  secondCategory: string
+
+  //алиас - уникальный урл по которому будет открываться страница
+  @prop({ unique: true })
+  alias: string
+
+  @prop()
+  title: string
+
   //катагория страницы? не понял пока что
+  @prop()
   category: string
-  hh?: {
-    count: number
-    juniorSalary: number
-    middleSalary: number
-    seniorSalary: number
-  }
-  advantages: {
-    title: string
-    description: string
-  }[]
+
+  @prop({ type: () => [HhData] })
+  hh?: HhData
+
+  @prop({ type: () => [TopPageAdvantage] })
+  advantages: TopPageAdvantage[]
+
+  @prop()
   seoText: string
+
+  @prop()
   tagsTitle: string //? не обязательный мм?
+
+  @prop({ type: () => [String] })
   tags: string[]
 }
